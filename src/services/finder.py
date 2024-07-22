6aafca6731d2425ac01fc4a44f0be9e0d374dfa3
Sokolov Fedor <sokol_f4@mail.ru>
2024-07-22 14:53:27 +0300

    Add building response from docs result in finder

--- src/services/finder.py
+++ src/services/finder.py
@@ -1,6 +1,6 @@
 import aiohttp
 
-from src.routers.schemas import FinderResult, FindRequest
+from src.routers.schemas import DocumentSchema, FinderResult, FindRequest
 from src.services.base import BaseService
 
 
@@ -13,4 +13,21 @@ class FinderService(BaseService):
         async with aiohttp.ClientSession() as session:
             async with session.post(f"{self.base_url}/api/v1/find", json=payload.model_dump()) as response:
                 result = await response.json()
+                docs = [DocumentSchema(**doc) for doc in result["documents"]]
+                result["response"] = self._prepare_response(docs)
+
                 return FinderResult(**result).response
+
+    def _prepare_response(self, docs: list[DocumentSchema]) -> str:
+        response_lines = []
+        for doc in docs:
+            line = (
+                f"*Company*: {doc.company}\n"
+                f"*Industry*: {doc.industry}\n"
+                f"*Description*: {doc.description}\n"
+                f"*Tags*: {doc.tags}\n"
+                f"[Read More]({doc.s3_link})"
+            )
+            response_lines.append(line)
+
+        return "\n\n---\n\n".join(response_lines)
